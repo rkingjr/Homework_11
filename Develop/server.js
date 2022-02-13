2cb794d2eb962319b1bcf8f2f2a40c8276555779
Randy King Jr <rkingjr@outlook.com>
2022-02-13 15:36:57 -0600

    initial commit

---
+++ Develop/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const fs = require("fs");
+
+var app = express();
+var PORT = process.env.PORT || 3001
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.uss("/assets", express.static("./assets"));
+
+require("./routing/html-routes")(app);
+require("./routing/api-routes")(app);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
