b3d5bac5e5848b87c9666fe519889cd7f2ff0004
Randy King Jr <rkingjr@outlook.com>
2022-02-13 16:05:35 -0600

    update html-routes.js

--- Develop/routing/api-routes.js
+++ Develop/routing/api-routes.js
@@ -1,12 +1,12 @@
 const fs = require("fs");
-var data = JSON.parse(fs.readFileSync("./../db/db.json", "utf8"));
+var data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
 
 module.exports = function(app) {
     app.get("/api/notes", function(req, res) {
         res.json(data);
     });
     app.get("/api/notes/:id", function(req, res) {
-        res.json(data[Number(req,.parse.id)]);
+        res.json(data[Number(req,parse.id)]);
     });
     app.post("/api/notes", function(req,res) {
         let newNote = req.body;
