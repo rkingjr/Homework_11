2d5155115b6e2890f3b7927efc755d1b75654a3e
Randy King Jr <rkingjr@outlook.com>
2022-02-14 09:48:06 -0600

    removed files from develop foulder

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use("/assets", express.static("./assets"));
 
-require("./routing/html-routes")(app);
-require("./routing/api-routes")(app);
+require("./api/html-routes")(app);
+require("./api/api-routes")(app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
