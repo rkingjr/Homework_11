ed478d3f30b373ff4974a8d4536cefbe46fa1fb4
Randy King Jr <rkingjr@outlook.com>
2022-02-28 23:37:54 -0600

    update routes

--- api/html-routes.js
+++ api/html-routes.js
@@ -2,9 +2,9 @@ const path = require("path");
 
 module.exports = function(app) {
     app.get("/", function(req,res) {
-        res.sendFile(path.join(__dirname, "/../public/index.html"));
+        res.sendFile(path.join(__dirname, "/../index.html"));
     });
     app.get("/notes", function(req,res) {
-        res.sendFile(path.join(__dirname, "/../public/notes.html"));
+        res.sendFile(path.join(__dirname, "/../notes.html"));
     });
 }
